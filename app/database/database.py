589a341b1cdfca40e92e73f3bd1ca75fce8b6813
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-10-23 16:24:19 -0300

    fix: do not use model if k == 0

--- app/database/database.py
+++ app/database/database.py
@@ -3,17 +3,18 @@ import random
 from sentence_transformers import SentenceTransformer
 from typing import List, Tuple
 
-#sentence-transformers/all-MiniLM-L6-v2
-#jaimevera1107/all-MiniLM-L6-v2-similarity-es
+# sentence-transformers/all-MiniLM-L6-v2
+# jaimevera1107/all-MiniLM-L6-v2-similarity-es
+
 
 class VectorialDatabase:
-    def __init__(self, model_url : str = "sentence-transformers/all-MiniLM-L6-v2"):
+    def __init__(self, model_url: str = "sentence-transformers/all-MiniLM-L6-v2"):
         # Initialize the Tweets and uuid containers.
         # As the vectorial database uses integers for indexing, a copy of the uuids is stored in a dictionary.
         self.uuid_hash = {}
         self.tweets = {}
 
-        #Initialize the model for embedding the tweets and the index for the Vect database.
+        # Initialize the model for embedding the tweets and the index for the Vect database.
         self.model = SentenceTransformer(model_url)
 
     def __initialize_index(self):
@@ -31,7 +32,12 @@ class VectorialDatabase:
 
     def rank_by_tweets_sample(self, tweets_sample: List[str], k: int = 3):
         # Make the limit value of k the same as the number of tweets in the database.
-        if k > len(self.tweets): k = (len(self.tweets) - 1)
+        if k > len(self.tweets):
+            k = len(self.tweets)
+
+        # If k is 0, no tweets are loaded so, self.index does not exist
+        if k == 0:
+            return {}
 
         if len(tweets_sample) == 0:
             # Return random twits due to the lack of information of the user.
