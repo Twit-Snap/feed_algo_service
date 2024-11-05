bbdd5c5fa677a443075d87d513c48c7b4056df8b
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-11-04 22:02:12 -0300

    feat: add tweets to index if len > 0 If len(request_data) > 0 add tweets to index

--- app/controllers/algo_controller.py
+++ app/controllers/algo_controller.py
@@ -1,6 +1,7 @@
 from models.Tweet import TweetRequest, TweetRanking, Tweet
 from database.database import vectorial_db
 
+
 class AlgoController:
     def __init__(self):
         self.__db = vectorial_db
@@ -9,7 +10,9 @@ class AlgoController:
         request_data = []
         for tweet_data in request.data:
             request_data.append((tweet_data.id, tweet_data.content))
-        self.__db.add_tweets_to_index(request_data)
+
+        if len(request_data) > 0:
+            self.__db.add_tweets_to_index(request_data)
 
     def rank_by_given_tweets(self, request: TweetRequest) -> TweetRanking:
         print("Sample in the request: ", request.data)
@@ -21,7 +24,9 @@ class AlgoController:
             tweets_received.append(tweet_data.content)
         limit = request.limit
         rank_data = self.__db.rank_by_tweets_sample(tweets_received, limit)
-        tweets_data = [Tweet(id=tweet_id, content=tweet_content) for tweet_id, tweet_content in rank_data.items()]
+        tweets_data = [Tweet(id=tweet_id, content=tweet_content)
+                       for tweet_id, tweet_content in rank_data.items()]
         return TweetRanking(data=tweets_data)
+
 
 algo_controller = AlgoController()
